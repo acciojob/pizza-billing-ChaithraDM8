3d99c753dc02640d3375281c06acb36ce7822a89
Chaithra <chaithradm8@gmail.com>
2022-11-27 12:49:00 +0530

    Pizza class implimented

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,10 +4,14 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill="";
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg)
+            price=300;
+        else
+            price=400;
         // your code goes here
     }
 
@@ -16,19 +20,26 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
+        price+=80;
         // your code goes here
     }
 
     public void addExtraToppings(){
+        if(isVeg==true)
+            price+=70;
+        else
+            price+=120;
         // your code goes here
     }
 
     public void addTakeaway(){
-        // your code goes here
+        price+=20;
+
     }
 
     public String getBill(){
         // your code goes here
+       bill+=price;
         return this.bill;
     }
 }
